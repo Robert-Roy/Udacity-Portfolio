cce25fb907b199de56cae5c00082c4d06870ff7e
Robert-Roy <robertproy@live.com>
2017-09-04 16:47:53 -0500

    add basic layout for portfolio project

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Robert's Portfolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class ="row">
+                <div class="col-sm-1">
+                    <img src="img/Calmer Terrible Face.png">
+                </div>
+                <div class="col-sm-5 text-right pull-right">
+                    <h1>ROBERT ROY</h1>
+                    <h4>WEB DEVELOPER</h4>
+                </div>
+
+            </div>
+            <hr>
+            <div class ="row">
+                <img class="img-responsive" src="img/publicdomainprogramming.jpg" alt="Visual abstraction of programming">
+            </div>
+            <div class ="row">
+                <h3>Featured Work</h3>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="img/robertsworkspace.png" alt="Roberts Workspace">
+                    <h4>Robert's Workspace</h4>
+                    <h6><a href='https://robertsworkspace.com'>RobertsWorkspace.com</a></h6>
+
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="img/numbersintowords.png" alt="Numbers Into Words">
+                    <h4>Numbers Into Words</h4>
+                    <h6><a href='http://numbersintowords.com'>NumbersIntoWords.com</a></h6>
+
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="img/lightspeed.png" alt="Light Speed">
+                    <h4>Light Speed</h4>
+                    <h6><a href='http://lightspeed.robertsworkspace.com/'>lightspeed.js</a></h6>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
