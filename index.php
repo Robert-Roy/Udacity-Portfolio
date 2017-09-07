ae5ad343949c4c15acce1e59c15e3c18299712bf
Robert-Roy <robertproy@live.com>
2017-09-06 20:06:56 -0500

    update formatting

--- index.php
+++ index.php
@@ -5,44 +5,47 @@
         <title>Robert's Portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/styles.css">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link href="//fonts.googleapis.com/css?family=Abel" rel="stylesheet">
     </head>
     <body>
         <div class="container">
             <div class ="row">
-                <div class="col-sm-1">
-                    <img src="img/Calmer Terrible Face.png">
+                <div class="col-xs-5">
+                    <img id="portrait" class="img-responsive" src="img/Calmer Terrible Face.png">
                 </div>
-                <div class="col-sm-5 text-right pull-right">
-                    <h1>ROBERT ROY</h1>
-                    <h4>WEB DEVELOPER</h4>
+                <div class="col-xs-7 text-right">
+                    <h1 id="name">ROBERT ROY</h1>
+                    <h4 id="profession">WEB DEVELOPER</h4>
                 </div>
-
             </div>
             <hr>
             <div class ="row">
-                <img class="img-responsive" src="img/publicdomainprogramming.jpg" alt="Visual abstraction of programming">
+                <div class="col-sm-12">
+                    <img class="img-responsive" src="img/publicdomainprogramming.jpg" alt="Visual abstraction of programming">
+                </div>
             </div>
             <div class ="row">
-                <h3>Featured Work</h3>
+                <div class="col-sm-12">
+                    <h3 class="section-header">My Work</h3>
+                </div>
             </div>
             <div class="row">
                 <div class="col-sm-4 text-center">
                     <img class="img-responsive" src="img/robertsworkspace.png" alt="Roberts Workspace">
-                    <h4>Robert's Workspace</h4>
-                    <h6><a href='https://robertsworkspace.com'>RobertsWorkspace.com</a></h6>
+                    <h3 class="caption">Robert's Workspace</h3>
+                    <p><a href='https://robertsworkspace.com'>RobertsWorkspace.com</a></p>
 
                 </div>
                 <div class="col-sm-4 text-center">
                     <img class="img-responsive" src="img/numbersintowords.png" alt="Numbers Into Words">
-                    <h4>Numbers Into Words</h4>
-                    <h6><a href='http://numbersintowords.com'>NumbersIntoWords.com</a></h6>
-
+                    <h3 class="caption">Numbers Into Words</h3>
+                    <p><a href='http://numbersintowords.com'>NumbersIntoWords.com</a></p>
                 </div>
                 <div class="col-sm-4 text-center">
                     <img class="img-responsive" src="img/lightspeed.png" alt="Light Speed">
-                    <h4>Light Speed</h4>
-                    <h6><a href='http://lightspeed.robertsworkspace.com/'>lightspeed.js</a></h6>
+                    <h3 class="caption">Light Speed</h3>
+                    <p><a href='http://lightspeed.robertsworkspace.com/'>lightspeed.js</a></p>
                 </div>
             </div>
         </div>
